7dc0d8ed4223e77d277a155f2385f1927d8a122d
Krissy Do <krissytdo@hotmail.com>
2020-05-12 21:21:20 -0700

    Added reset button, refactored event handler
    added toggleplayer function

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,10 +1,39 @@
-//list of cells element
-var cells = document.querySelectorAll('td');
+//list of cells elements
+var squares = document.querySelectorAll('td');
 
+var game = {piece: [[0,0,0], [0, 0, 0]]};
+
+var player;
+
+var togglePlayer = () => {
+  if(player) {
+    player = false;
+  } else {
+    player = true;
+  }
+};
 
 //event listener to mark an X on cells when clicked
-cells.forEach( (cell) =>{
-  cell.addEventListener("click", (event) => {
-    event.target.innerText = 'X';
+squares.forEach( (square) =>{
+  square.addEventListener("click", (event) => {
+    if(!event.target.innerText) {
+      if(player) {
+        togglePlayer();
+        event.target.innerText = 'X';
+      } else {
+        togglePlayer();
+        event.target.innerText = 'O';
+      }
+    }
   });
+});
+
+//event listener for reset button
+var button = document.querySelector('button');
+button.addEventListener("click", (event) => {
+  player = false;
+  squares.forEach( (square) => {
+    square.innerText = null;
+    console.log(square.innerText);
+  })
 });
