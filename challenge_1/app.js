389c612ce9aeb72498323826a11e74888e5e7b3c
Krissy Do <krissytdo@hotmail.com>
2020-05-12 19:51:44 -0700

    Added event listener for X

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,1 +1,10 @@
-var xOnClick
+//list of cells element
+var cells = document.querySelectorAll('td');
+
+
+//event listener to mark an X on cells when clicked
+cells.forEach( (cell) =>{
+  cell.addEventListener("click", (event) => {
+    event.target.innerText = 'X';
+  });
+});
