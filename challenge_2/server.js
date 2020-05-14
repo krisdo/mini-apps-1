58d073253f392b3dcecd777ac86a026643639195
Krissy Do <krissytdo@hotmail.com>
2020-05-13 23:09:18 -0700

    Populated html and setup server
    working on post request

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,35 @@
+const express = require('express');
+const path = require('path');
+const port = 3000;
+const app = express();
+
+app.use(express.static(path.join(__dirname,'client')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(port, () =>{
+  console.log( `listening ${port}`);
+});
+
+app.get('/', (req, res) => {
+  
+  // res.sendFile(__dirname + '/client/index.html');
+  
+ 
+});
+
+app.post('/upload_json', (req, res) => {
+  // if (err) throw err;
+ var body = JSON.stringify(req.body)
+  // body.split('\"');
+  console.log(body);
+});
 
 
+// const express = require('express')
+// const app = express()
+// const port = 3000
+
+// app.get('/', (req, res) => res.send('Hello World!'))
+
+// app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
