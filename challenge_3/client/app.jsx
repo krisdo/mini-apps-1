47137c1eabd1d5fbbd0f931958204575f4dcbb68
Krissy Do <krissytdo@hotmail.com>
2020-06-02 14:08:44 -0700

    setup pkg json react server

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,22 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+class Grocery extends React.Component {
+
+  constructor (props) {
+    super(props)
+  }
+
+  render() {
+    return <div><h1>hello world</h1></div>
+  }
+   
+};
+
+export default Grocery;
+
+ReactDOM.render(element, document.getElementById('App'));
+
+
 
 
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,10 @@
+const express = require('express');
+const app = express();
 
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+const port = 3000;
+app.listen(port, () => console.log(`listening to ${port}`));
 
