48a023bc006091877f33494c7c2b793599608c00
Krissy Do <krissytdo@hotmail.com>
2020-06-11 14:32:09 -0700

    add more changes

--- challenge_4/client/src/app.jsx
+++ challenge_4/client/src/app.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Square from '.components/Square.jsx';
+// import Square from '.components/Square.jsx';
 
 class ConnectFour extends React.Component {
 
@@ -10,14 +10,15 @@ class ConnectFour extends React.Component {
   }
 
   render(){
-    <Square x={0} y={0}/><Square x={1} y={0}/><Square x={2} y={0}/><Square x={3} y={0}/><Square x={4} y={0}/><Square x={5} y={0}/><Square x={6} y={0}/>
-    <Square x={0} y={1}/><Square x={1} y={1}/><Square x={2} y={1}/><Square x={3} y={1}/><Square x={4} y={1}/><Square x={5} y={1}/><Square x={6} y={1}/>
-    <Square x={0} y={2}/><Square x={1} y={2}/><Square x={2} y={2}/><Square x={3} y={2}/><Square x={4} y={2}/><Square x={5} y={2}/><Square x={6} y={2}/>
-    <Square x={0} y={3}/><Square x={1} y={3}/><Square x={2} y={3}/><Square x={3} y={3}/><Square x={4} y={3}/><Square x={5} y={3}/><Square x={6} y={3}/>
-    <Square x={0} y={4}/><Square x={1} y={4}/><Square x={2} y={4}/><Square x={3} y={4}/><Square x={4} y={4}/><Square x={5} y={4}/><Square x={6} y={4}/>
-    <Square x={0} y={5}/><Square x={1} y={5}/><Square x={2} y={5}/><Square x={3} y={5}/><Square x={4} y={5}/><Square x={5} y={5}/><Square x={6} y={5}/>
+    return(<div>testing</div>);
+    // <Square x={0} y={0}/><Square x={1} y={0}/><Square x={2} y={0}/><Square x={3} y={0}/><Square x={4} y={0}/><Square x={5} y={0}/><Square x={6} y={0}/>
+    // <Square x={0} y={1}/><Square x={1} y={1}/><Square x={2} y={1}/><Square x={3} y={1}/><Square x={4} y={1}/><Square x={5} y={1}/><Square x={6} y={1}/>
+    // <Square x={0} y={2}/><Square x={1} y={2}/><Square x={2} y={2}/><Square x={3} y={2}/><Square x={4} y={2}/><Square x={5} y={2}/><Square x={6} y={2}/>
+    // <Square x={0} y={3}/><Square x={1} y={3}/><Square x={2} y={3}/><Square x={3} y={3}/><Square x={4} y={3}/><Square x={5} y={3}/><Square x={6} y={3}/>
+    // <Square x={0} y={4}/><Square x={1} y={4}/><Square x={2} y={4}/><Square x={3} y={4}/><Square x={4} y={4}/><Square x={5} y={4}/><Square x={6} y={4}/>
+    // <Square x={0} y={5}/><Square x={1} y={5}/><Square x={2} y={5}/><Square x={3} y={5}/><Square x={4} y={5}/><Square x={5} y={5}/><Square x={6} y={5}/>
   }
 }
 
 
-ReactDOM.render(<ConnectFour />, Document.getElementById('app'));
+ReactDOM.render(<ConnectFour />, document.getElementById('app'));
--- challenge_4/server.js
+++ challenge_4/server.js
@@ -6,6 +6,6 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('./client/dist'));
 
-app.listen(3000, () => {
-  console.log('Listening at 3000');
+app.listen(3001, () => {
+  console.log('Listening at 3001');
 })
