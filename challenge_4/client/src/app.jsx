50aa6ad5bdfe95939c1935b06522867fb6a0b0d9
Krissy Do <krissytdo@hotmail.com>
2020-06-10 21:58:35 -0700

    Completed Setup and started ConnectFour app component

---
+++ challenge_4/client/src/app.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class ConnectFour extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+  }
+
+  render(){
+    <Square x={0} y={0}/><Square x={1} y={0}/><Square x={2} y={0}/><Square x={3} y={0}/><Square x={4} y={0}/><Square x={5} y={0}/><Square x={6} y={0}/>
+    <Square x={0} y={1}/><Square x={1} y={1}/><Square x={2} y={1}/><Square x={3} y={1}/><Square x={4} y={1}/><Square x={5} y={1}/><Square x={6} y={1}/>
+    <Square x={0} y={2}/><Square x={1} y={2}/><Square x={2} y={2}/><Square x={3} y={2}/><Square x={4} y={2}/><Square x={5} y={2}/><Square x={6} y={2}/>
+    <Square x={0} y={3}/><Square x={1} y={3}/><Square x={2} y={3}/><Square x={3} y={3}/><Square x={4} y={3}/><Square x={5} y={3}/><Square x={6} y={3}/>
+    <Square x={0} y={4}/><Square x={1} y={4}/><Square x={2} y={4}/><Square x={3} y={4}/><Square x={4} y={4}/><Square x={5} y={4}/><Square x={6} y={4}/>
+    <Square x={0} y={5}/><Square x={1} y={5}/><Square x={2} y={5}/><Square x={3} y={5}/><Square x={4} y={5}/><Square x={5} y={5}/><Square x={6} y={5}/>
+  }
+}
+
+
+ReactDOM.render(<ConnectFour />, Document.getElementById('app'));
--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,2 +1,11 @@
+const express = require('express');
 
+const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('./client/dist'));
+
+app.listen(3000, () => {
+  console.log('Listening at 3000');
+})
