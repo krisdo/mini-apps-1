e294b6a34d9da8d33ffc90ad631b0e1515673a4c
Krissy Do <krissytdo@hotmail.com>
2020-07-01 15:55:46 -0700

    added css. looks like the game

--- challenge_4/client/src/app.jsx
+++ challenge_4/client/src/app.jsx
@@ -1,22 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import Square from '.components/Square.jsx';
+import Square from './components/Square.jsx';
+import {Container, Row, Col, Button} from 'react-bootstrap';
+
 
 class ConnectFour extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      playerTurn: 1
+    }
 
   }
 
+  clickSquare() {
+    //send post request to server with coordinates
+    
+  }
+
   render(){
-    return(<div>testing</div>);
-    // <Square x={0} y={0}/><Square x={1} y={0}/><Square x={2} y={0}/><Square x={3} y={0}/><Square x={4} y={0}/><Square x={5} y={0}/><Square x={6} y={0}/>
-    // <Square x={0} y={1}/><Square x={1} y={1}/><Square x={2} y={1}/><Square x={3} y={1}/><Square x={4} y={1}/><Square x={5} y={1}/><Square x={6} y={1}/>
-    // <Square x={0} y={2}/><Square x={1} y={2}/><Square x={2} y={2}/><Square x={3} y={2}/><Square x={4} y={2}/><Square x={5} y={2}/><Square x={6} y={2}/>
-    // <Square x={0} y={3}/><Square x={1} y={3}/><Square x={2} y={3}/><Square x={3} y={3}/><Square x={4} y={3}/><Square x={5} y={3}/><Square x={6} y={3}/>
-    // <Square x={0} y={4}/><Square x={1} y={4}/><Square x={2} y={4}/><Square x={3} y={4}/><Square x={4} y={4}/><Square x={5} y={4}/><Square x={6} y={4}/>
-    // <Square x={0} y={5}/><Square x={1} y={5}/><Square x={2} y={5}/><Square x={3} y={5}/><Square x={4} y={5}/><Square x={5} y={5}/><Square x={6} y={5}/>
+    return(
+      <Container width={400} className='board text-center'>
+        <h1 className='text-center'>Connect Four</h1>
+        <Row><Col className='px-0' ><Square x={0} y={0}/></Col><Col className='px-0'><Square x={1} y={0}/></Col><Col className='px-0'><Square x={2} y={0}/></Col><Col className='px-0'><Square x={3} y={0}/></Col ><Col className='px-0'><Square x={4} y={0}/></Col><Col className='px-0'><Square x={5} y={0}/></Col><Col className='px-0'><Square x={6} y={0}/></Col></Row>
+        <Row ><Col className='px-0'><Square x={0} y={1}/></Col><Col className='px-0'><Square x={1} y={1}/></Col><Col className='px-0'><Square x={2} y={1}/></Col><Col className='px-0'><Square x={3} y={1}/></Col><Col className='px-0'><Square x={4} y={1}/></Col><Col className='px-0'><Square x={5} y={1}/></Col><Col className='px-0'><Square x={6} y={1}/></Col></Row>
+        <Row ><Col className='px-0'><Square x={0} y={2}/></Col><Col className='px-0'><Square x={1} y={2}/></Col><Col className='px-0'><Square x={2} y={2}/></Col><Col className='px-0'><Square x={3} y={2}/></Col><Col className='px-0'><Square x={4} y={2}/></Col><Col className='px-0'><Square x={5} y={2}/></Col><Col className='px-0'><Square x={6} y={2}/></Col></Row>
+        <Row ><Col className='px-0'><Square x={0} y={3}/></Col><Col className='px-0'><Square x={1} y={3}/></Col><Col className='px-0'><Square x={2} y={3}/></Col><Col className='px-0'><Square x={3} y={3}/></Col><Col className='px-0'><Square x={4} y={3}/></Col><Col className='px-0'><Square x={5} y={3}/></Col><Col className='px-0'><Square x={6} y={3}/></Col></Row>
+        <Row ><Col className='px-0'><Square x={0} y={4}/></Col><Col className='px-0'><Square x={1} y={4}/></Col><Col className='px-0'><Square x={2} y={4}/></Col><Col className='px-0'><Square x={3} y={4}/></Col><Col className='px-0'><Square x={4} y={4}/></Col><Col className='px-0'><Square x={5} y={4}/></Col><Col className='px-0'><Square x={6} y={4}/></Col></Row>
+        <Row ><Col className='px-0'><Square x={0} y={5}/></Col><Col className='px-0'><Square x={1} y={5}/></Col><Col className='px-0'><Square x={2} y={5}/></Col><Col className='px-0'><Square x={3} y={5}/></Col><Col className='px-0'><Square x={4} y={5}/></Col><Col className='px-0'><Square x={5} y={5}/></Col><Col className='px-0'><Square x={6} y={5}/></Col></Row>
+        <Button className='text-center mr-1'>Restart</Button>
+      </Container>
+    )
   }
 }
 
--- challenge_4/client/src/components/Square.jsx
+++ challenge_4/client/src/components/Square.jsx
@@ -1,1 +1,69 @@
 import React from 'react';
+import {Container, Col} from 'react-bootstrap';
+import styled from 'styled-components';
+
+const StyledSquare = styled.div`
+  background-color: blue;
+  width: 100%;
+  padding: 10%;
+  `;
+
+const StyledCircleBlank = styled.div`
+  border-radius:  50%;
+  background-color: white;
+  width: 90%;
+  padding-bottom: 90%;
+`;
+const StyledCircleRed = styled.div`
+  border-radius:  50%;
+  background-color: red;
+  width: 90%;
+  padding-bottom: 90%;
+`;
+const StyledCircleYellow = styled.div`
+  border-radius:  50%;
+  background-color: yellow;
+  width: 90%;
+  padding-bottom: 90%;
+`;
+
+
+
+class Square extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      view: 'blank'
+    }
+  }
+
+  onClick(e) {
+  
+    if(props.player === 1) {
+      this.setState({
+        view: 'red'
+      });
+    } else {
+      this.setState({
+        view: 'yellow'
+      });
+    }
+
+  }
+
+  render(){
+
+    return(
+  
+     <StyledSquare className='square'>
+      <StyledCircle className='circle'></StyledCircle>
+      </StyledSquare>   
+
+  
+    )
+
+  }
+}
+
+export default Square;
